193be100e3d9ed83b86afaa9c9e951d9d3f8b526
Emre MENTEŞE <emrementese@gmail.com>
2021-01-04 18:38:41 +0300

    Add files via upload

---
+++ Basic Decision Tree/fileop.py
@@ -0,0 +1,16 @@
+'''
+- File Operation for Decision Tree -
+
+-> Created by Emre MENTEŞE on 04.01.2021
+-> Copyright © 2021 Emre MENTEŞE. All rights reserved.
+
+All file operation functions for basic decision tree and Apple Class for data.
+
+'''
+
+#Apple Class for data
+class Apple():
+
+    pass
+
+
