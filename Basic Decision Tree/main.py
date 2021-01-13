1f947ec97a2198c75ca01798c5def7a79cc2df11
Emre MENTEŞE <emrementese@gmail.com>
2021-01-13 18:38:29 +0300

    Update main.py

--- Basic Decision Tree/main.py
+++ Basic Decision Tree/main.py
@@ -4,13 +4,19 @@
 -> Created by Emre MENTEŞE on 04.01.2021
 -> Copyright © 2021 Emre MENTEŞE. All rights reserved.
 
+- Extraction data - Root Node - Branching -
+
 '''
 
+import time
+import os
 import fileop
 from threading import Thread
 
-print("--- Welcome Basic Decision Tree ---")
+print("\n--- Welcome Basic Decision Tree ---")
 traindata = fileop.filextraction()
-
 #data count - train: 150 test: 22801
-print(f"Finded data count: {len(traindata)}")
+print(f"Train data count finded: {len(traindata)} | Train is starting...")
+
+#Branching - traindata is root data.
+
