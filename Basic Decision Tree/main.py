07108740d4d9364a9e8526cf06e9ab78bfcaac64
Emre MENTEŞE <emrementese@gmail.com>
2021-01-17 01:07:20 +0300

    Update main.py

--- Basic Decision Tree/main.py
+++ Basic Decision Tree/main.py
@@ -23,5 +23,8 @@ print(f"Train data count finded: {len(traindata)} | Train is starting...")
 #control the entropi function
 #print(iteration.entropi(34,23,56))
 
+#control the informaton gain function
+#print(iteration.infogain(30,20,40,10,25,25))
+
 #Branching - traindata is root data.
 
