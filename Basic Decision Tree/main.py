c482ca8b77a15f4c03c1b793c7bb922f9aaf0ed8
Emre MENTEŞE <emrementese@gmail.com>
2021-01-17 11:58:52 +0300

    Update main.py

--- Basic Decision Tree/main.py
+++ Basic Decision Tree/main.py
@@ -14,6 +14,7 @@ import fileop
 import iteration
 from threading import Thread
 
+
 print("\n--- Welcome Basic Decision Tree ---")
 traindata = fileop.filextraction()
 #data count - train: 150 test: 22801
